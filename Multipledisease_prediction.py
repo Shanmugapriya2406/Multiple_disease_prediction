686ccd43c3c53577cdd1bfb369a5be321dbf02a2
Shanmugapriya <annpriya2406@gmail.com>
2025-02-07 06:06:53 +0530

    Add files via upload

---
+++ Multipledisease_prediction.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.image("C:/Users/91701/Downloads/mdps.jpeg", width=600 )
+st.write("🩺 MULTIPLE DISEASE PRDICTION SYSTEM")
+st.write('''Welcome to the Multiple Disease Prediction System. 
+         A Multiple Disease Prediction System is an AI-powered web application that predicts the likelihood of different diseases (e.g., Parkinson, Chronic Kidney disease, Liver disease) 
+         based on user-inputted medical parameters. It uses machine learning models trained on medical datasets to classify whether a person is at risk of a specific disease.
+         Please select a disease prediction model from the sidebar.''')
+
